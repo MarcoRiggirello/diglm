649611a1edece7c29bd7f7cc7578f2f03cf668cf
Marco <marco.riggirello@libero.it>
2022-03-23 14:35:49 +0100

    code refactoring dividing code in 'spqr.py' and 'utils.py' was useless. Much more clear now (IMHO).

--- SpQR-Flow/spqr.py
+++ SpQR-Flow/spqr.py
@@ -1,25 +1,135 @@
 # Marco Riggirello & Antoine Venturini
+from tensorflow import function, Module
+from tensorflow.python.keras.layers import Layer, Dense, Reshape
+from tensorflow.python.keras.activations import softmax, softplus
+from tensorflow_probability.python.bijectors import Bijector, RealNVP, Chain, RationalQuadraticSpline
 
-from tensorflow_probability.python.bijectors import Bijector, RealNVP, Chain
+class SplineBlock(Layer):
+    """ SplineBlock class
+    """
+    def __init__(self,
+                 hidden_layers=[512,512]):
+        super().__init__()
+        self._layers = [
+            Dense(n,activation="relu",name=f"spqr_nn_layer_{i}")
+            for i,n in enumerate(hidden_layers)
+        ]
 
-from utils import SplineInitializer
+    def call(self, units):
+        for layer in self._layers:
+            units = layer(units)
+        return units
+
+
+class BinsLayer(Layer):
+    """ BinsLayer class
+    """
+    def __init__(self,
+                 nunits,
+                 nbins,
+                 border,
+                 min_bin_gap=1e-3,
+                 name="bins"):
+        super().__init__(name=name)
+        self._nunits = nunits
+        self._nbins = nbins
+        self._border = border
+        self._min_bin_gap = min_bin_gap
+        self._dense = Dense(self._nunits * self._nbins, activation=softmax)
+        self._reshape = Reshape((-1, self._nunits * self._nbins))
+
+    def call(self, units):
+        units = self._dense(units)
+        units = self._reshape(units)
+        return units * (2 * self._border - self._nbins * self._min_bin_gap ) - self._min_bin_gap
+
+
+class SlopesLayer(Layer):
+    """ SlopesLayer class
+    """
+    def __init__(self,
+                 nunits,
+                 nbins,
+                 min_slope=1e-3,
+                 name="slopes"):
+        super().__init__(name=name)
+        self._nunits = nunits
+        self._nslopes = nbins - 1
+        self._min_slope = min_slope
+        self._dense = Dense( self._nunits * self._nslopes, activation=softplus)
+        self._reshape = Reshape((-1, self._nunits * self._nslopes))
+
+    def call(self, units):
+        units = self._dense(units)
+        units = self._reshape(units)
+        return units + self._min_slope
+
+
+class SplineInitializer(Module):
+    """ SplineInitializer class
+    """
+    def __init__(self,
+                 nbins=128,
+                 border=4,
+                 hidden_layers=[512,512],
+                 min_bin_gap=1e-3,
+                 min_slope=1e-3):
+        super().__init__()
+        self._nbins = nbins
+        self._border = border
+        self._nn = SplineBlock(hidden_layers=hidden_layers)
+        self._min_bin_gap = min_bin_gap
+        self._min_slope = min_slope
+        self._built = False
+        #self._bin_widths = None
+        #self._bin_heights = None
+        #self._knot_slopes = None
+
+    @function
+    def __call__(self,
+                 x,
+                 nunits):
+        if not self._built:
+            self._bin_widths = BinsLayer(nunits,
+                                         self._nbins,
+                                         self._border,
+                                         min_bin_gap=self._min_bin_gap,
+                                         name="w" )
+            self._bin_heights = BinsLayer(nunits,
+                                          self._nbins,
+                                          self._border,
+                                          min_bin_gap=self._min_bin_gap,
+                                          name="h" )
+            self._knot_slopes = SlopesLayer(nunits,
+                                            self._nbins,
+                                            min_slope=self._min_slope,
+                                            name="s" )
+            self._built = True
+        x = self._nn(x)
+        return RationalQuadraticSpline(self._bin_widths(x),
+                                       self._bin_heights(x),
+                                       self._knot_slopes(x),
+                                       range_min= -self._border)
 
 class NeuralSplineFlow(Bijector):
+    """ Neural Spline Flow bijector.
+    """
     def __init__(self,
                  splits=None,
                  masks=None,
-                 spline_fn=SplineInitializer(),
-                 name="neural_spline_flow"
+                 **spline_kwargs
                  ):
-        super().__init__(forward_min_event_ndims=0,
-                         name=name)
-        if splits is not None and masks is not None:
-            raise ValueError("You can specify `splits` OR `masks`, not both.")
+        super().__init__(forward_min_event_ndims=1, name="nsf")
         if splits is None and masks is None:
             raise ValueError("You must specify 'splits' OR 'masks'.")
-        self._spline_fn = spline_fn
+        if splits is not None and masks is not None:
+            raise ValueError("You can specify `splits` OR `masks`, not both.")
+        self._spline_fn = SplineInitializer(**spline_kwargs)
         self._coupling_layers = []
         if splits is not None:
+            if splits < 2:
+                raise ValueError("nsplits must be greater than or equal to 2 ",
+                                 "(You must split you feature vec in at least two parts).")
             for i in range(1,splits):
                 self._coupling_layers.append(RealNVP(fraction_masked=i/splits,
                                                          bijector_fn=self._spline_fn))
