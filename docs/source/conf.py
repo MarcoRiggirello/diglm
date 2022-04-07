75ddad4bc68bb4879c31937beeb4cd54c4012135
Antoine Venturini <83359491+AntuVenciu@users.noreply.github.com>
2022-04-07 17:03:36 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,13 +15,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
->>>>>>> origin/documentation
-
-
 # -- Project information -----------------------------------------------------
 
 project = 'SpQR-Flow'
