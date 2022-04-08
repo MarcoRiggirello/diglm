f804fefb537af270a97bb45d98b2c3a152d64ad0
AntuVenciu <antoinev151@gmail.com>
2022-04-08 15:44:04 +0200

    changes in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,12 +15,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
->>>>>>> origin/documentation
-
 
 # -- Project information -----------------------------------------------------
 
@@ -71,12 +65,8 @@ exclude_patterns = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-<<<<<<< HEAD
+
 html_theme = 'alabaster'
-=======
-html_theme = 'sphinx_rtd_theme'
->>>>>>> origin/documentation
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
